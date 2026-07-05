bca8785b217935f1fd1cba34fe7c584ca79cfc47
ICN <icn20260690@gmail.com>
2026-07-05 16:13:26 +0900

    Add Vercel Analytics

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      <Analytics />
     </QueryClientProvider>
   </React.StrictMode>,
 );
